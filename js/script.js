9c454e2bc63426716d9050a09d71ceb79c93117a
Michal Idzik <midzik826@gmail.com>
2018-01-24 21:26:07 +0000

    Finished Product

--- js/script.js
+++ js/script.js
@@ -5,35 +5,67 @@
 // CREATE THE FUNCTIONS BELOW
 
 // Document Ready Function. All of your jQuery should go in here. 
-$(document).ready(function() {
- $("#button").click(function() {
-  var response = $("#answer").val();
-  var pigMessage = sentenceToPigLatin(response);
-  $("#result").text(pigMessage);
- });
-  function isVowelFirstLetter(word) {
-   var last=response.slice(0, 1);
-   var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
-   for (i = 0; i < vowels.length; i = i + 1) {
-    if (vowels[i] === word[0]) {
-     return true;
-    }
-   }
-   return false;
-  }
+$(document).ready(function(){
+	$("#button").click(function() {
+	var response = $("#answer").val();
+	var latin = isVowelFirstLetter(response);
+  });
   
   
- function sentenceToPigLatin(words) {
-  if (isVowelFirstLetter(words[0])) {
-  return  words[0] + "ay";
-  }
-  else {
-  return words.slice(word.length-1);
-  }
- }
+   function isVowelFirstLetter(response) {
+	
+	var vowels = ['a', 'e', 'i', 'o', 'u', 'y','A', 'E', 'I', 'O', 'U', 'Y'];
+	 
+	 var last = response.split("");
 
+	 var includeV = vowels.includes(last[0]);    
+
+	 if(includeV === true){
+					
+        $("#result").text(response + "ay");
+
+    }else{
+	 
+	 
+	 var consonateShift = last.shift();
+
+	var consonatePush = last.push(consonateShift);
+
+	var consonateJoin = last.join(''); 
+
+	 $("#result").text(consonateJoin + "ay");
+	   }
+	
+    }
+
+   
+ //$("#button").click(function() {
+ // var response = $("#answer").val();
+ // var pigMessage = sentenceToPigLatin(response);
+ // $("#result").text(pigMessage);
+ //});
+ // function isVowelFirstLetter(word) {
+ //  var last = response.slice(0, 1);
+ //  var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+ //  for (i = 0; i < vowels.length; i = i + 1) {
+ //   if (vowels[i] === word[0]) {
+ //    return true;
+ //   }
+ //  }
+ //  return false;
+ // }
+  // function sentenceToPigLatin(words) {
+  //  if (isVowelFirstLetter(words[0])) {
+  //  return  words[0] + "ay";
+  //  }
+  //   else {
+  //  return words.slice(word.length-1);
+  //  }
+ 
+  // }
 
 });
+
 
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
